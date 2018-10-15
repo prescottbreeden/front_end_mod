3800c81c22f88e597fa97b5fe48d17532534eec2
prescottbreeden <prescott.breeden@gmail.com>
2018-10-15 16:50:37 -0700

    custom state engine started

---
+++ typescript_webpack/src/js/components/User.ts
@@ -0,0 +1,16 @@
+export class User {
+    private firstName: string;
+    private lastName: string;
+    public age: number;
+
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+
+    
+    
+
+}
---
+++ typescript_webpack/src/js/index.ts
@@ -0,0 +1,4 @@
+import { User } from './components/User';
+
+let bob = new User('Bob', 'Sagat', 61);
+console.log(bob)
